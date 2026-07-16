9afe91e2575e8b6107351222c9d91f0a29970d74
emredersiniz16 <156595390+emredersiniz16@users.noreply.github.com>
2026-07-16 16:04:38 +0300

    Update idle_engine.c

--- core/engines/idle_engine.c
+++ core/engines/idle_engine.c
@@ -1,3 +1,4 @@
+// core/engines/idle_engine.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,19 +9,23 @@
 
 // 1. HAYALET GEÇİŞİ EFEKTİ (Ghost Stream)
 void trigger_ghost_stream(int w, int h) {
-    printf("[IDLE] Hayalet veri akışı tetiklendi...\n");
+    // printf("[IDLE] Hayalet veri akışı tetiklendi...\n"); // Debug için kapatılabilir
     
     for(int i = 0; i < 5; i++) {
-        int random_y = rand() % h;
+        int random_y = rand() % (h > 0 ? h : 20); // Sıfıra bölme hatasını önlemek için güvenlik
         printf("\033[%d;0H\033[1;36m[SYS_DATA_STREAM_OK] 0x%X -> GLITCH_ACTIVE\033[0m", random_y, rand());
         fflush(stdout);
-        usleep(20000); 
+        usleep(200000); // 0.2 saniye ekranda kalsın
+        
+        // Ekranda çöp bırakmamak için yazıldığı satırı temizle
+        printf("\033[%d;0H\033[K", random_y); 
+        fflush(stdout);
     }
 }
 
 // 2. SİBER RADAR TARAMASI (Cyber Radar & Tech Terms)
 void trigger_cyber_radar(int w, int h) {
-    printf("[IDLE] Siber radar taraması başlatıldı...\n");
+    // printf("[IDLE] Siber radar taraması başlatıldı...\n");
     
     printf("\033[2;2H\033[1;32m⚡ SYS: ACTIVE\033[0m");
     printf("\033[3;2H\033[1;32m🔋 BAT: ONLINE\033[0m");
@@ -30,7 +35,8 @@ void trigger_cyber_radar(int w, int h) {
     int rastgele_indeks = rand() % 4; // 0 ile 3 arasında rastgele seçer
     
     // Seçilen kelimeyi ekranın üst köşesinde parlatır
-    printf("\033[2;%dH\033[1;36mCORE: %s\033[0m", w / 10 - 15, terimler[rastgele_indeks]);
+    int x_pos = (w > 150) ? (w / 10 - 15) : 50; // Terminal genişliğine göre güvenlik önlemi
+    printf("\033[2;%dH\033[1;36mCORE: %s\033[0m", x_pos, terimler[rastgele_indeks]);
     fflush(stdout);
     
     usleep(500000); // Yarım saniye ekranda parlasın
@@ -38,7 +44,7 @@ void trigger_cyber_radar(int w, int h) {
     // Temizleme adımı
     printf("\033[2;2H                ");
     printf("\033[3;2H                ");
-    printf("\033[2;%dH               ", w / 10 - 15);
+    printf("\033[2;%dH               ", x_pos);
     fflush(stdout);
 }
 
